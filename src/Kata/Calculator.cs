85dbbfebe62a22a1a20954c8d9b3cb34ca91c607
Carlos Solorzano <csolorzanoflores@gmail.com>
2020-09-29 10:36:36 -0600

    Ping

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -11,16 +11,8 @@ namespace Kata
             if(string.IsNullOrEmpty(number))
                 return 0;
 
-            List<string> numbers = number.Split(",").ToList();
-            int result = 0;
-            
-            foreach (string num in numbers)
-            {
-                int currentNumber = int.Parse(num);
-                result += currentNumber;
-            }
-
-            return result;
+            var numbers = number.Split(",").Select(int.Parse);
+            return numbers.Sum();
         }
     }
 }
